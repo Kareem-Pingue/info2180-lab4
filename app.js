f93426ef9ba1713ebe928cf0eae2b8cf6ce3ae24
Kareem Pingue <kareempingue@gmail.com>
2022-11-06 17:43:29 -0500

    Edited ALL 5 Files

---
+++ app.js
@@ -0,0 +1,28 @@
+//Kareem Pingue - 620146393
+//Javascript for Super Hero Page
+//Listener
+document.addEventListener('DOMContentLoaded', function()
+{
+    var button = document.getElementById("btn");
+    var url = 'http://localhost:8080/superheroes.php'
+    var key = document.getElementById("search-box").value;
+    
+    //Main
+    button.addEventListener('click', (event) => 
+    {
+        event.preventDefault(), httpR = new XMLHttpRequest();      
+        httpR.onreadystatechange = function()
+        {
+            if (httpR.readyState === XMLHttpRequest.DONE && httpR.status === 200)
+            {
+                var r = httpR.responseText; var key = document.getElementById("search-box").value; document.getElementById("result").innerHTML = r;
+            }
+            if (httpR.readyState === XMLHttpRequest.DONE && httpR.status === 404)
+            {
+                alert('ERROR - File not found.');
+            }
+        }
+        httpR.open('GET', "superheroes.php?q="+key, true);
+        httpR.send();
+    });    
+});
